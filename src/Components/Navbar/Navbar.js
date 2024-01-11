e4113bb6c4caf413188e08495d7599b31f51d739
Jaiten <Jaitensahu9@gmail.com>
2024-01-12 01:39:39 +0530

    added links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,8 +6,8 @@ const Navbar=()=>{
         <nav>
             <h2>Quote Generator</h2>
             <div className="icons">
-            <i className="fa-brands fa-github"></i>
-            <i className="fa-brands fa-linkedin"></i>
+            <a href="https://github.com/jaitensahu" target='_blank'><i className="fa-brands fa-github"></i></a>
+           <a href="https://www.linkedin.com/in/jaiten-sahu/" target='_blank'><i className="fa-brands fa-linkedin"></i></a>
 
             </div>
         </nav>
